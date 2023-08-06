4740b010174d1d17f5f600fdd425d98eac20c1ae
Thales Freitas Macedo <thalesfreitas96@gmail.com>
2023-08-06 02:16:01 -0300

    First working version

--- chain.py
+++ chain.py
@@ -1,66 +1,112 @@
-"""chain.f90 in Python."""
+import random as rng
 import numpy as np
 
 
-def random_distribution(
-    l_x: int,
-    sigma_u: float,
-    dist_kind: int,
-    rng: np.random.Generator,
+rng.seed(1892)
+PI = np.pi
+CI = complex(0, 1)
+
+
+def randomdistribution(
+    Lx: int,
+    sigmaU: float,
+    distkind: int,
 ):
-    """Random distribution of the on-site energies."""
-    if dist_kind == 1:
-        u_i = rng.normal(
-            scale=sigma_u,
-            size=l_x,
-        )
-    else:
-        u_i = rng.random(size=l_x)
-        u_i = sigma_u * (u_i - 0.5)
-    return u_i
+    U = np.zeros(Lx)
+    if distkind == 1:  #Gaussian
+        for i in range(Lx):
+            r1 = rng.random()
+            r2 = rng.random()
+            U[i] = sigmaU * np.cos(2 * PI * r1) * np.sqrt(- 2.0 * np.log(1.0 - r2))
+    else:  # uniform
+        for i in range(Lx):
+            U[i] = sigmaU * (rng.random() - 0.5)
+    return U
 
 
-def hamiltonian(
-    l_x: int,
-    n_ph: int,
-    sigma_u: float,
+def funcoesP(Nph: int):
+    P = np.zeros(shape=(Nph + 1, Nph + 1))
+    P[0, :] = np.ones(Nph + 1)
+
+    delta = np.zeros(shape=(Nph + 1, Nph + 1))
+    delta[0, 0] = 1.0
+
+    for M in range(1, Nph + 1):
+        delta[M, M] = 1.0
+        for j in range(1, M + 1):
+            P[j, M] = np.sqrt(M - (j - 1)) * P[j - 1, M]
+
+    return P, delta
+
+
+def Hamiltonian(
+    Lx: int,
+    Nph: int,
+    t: float,
+    gam: float,
+    sigmaU: float,
     omega: float,
-    dist_kind: int,
-    rng: np.random.Generator,
+    distkind: int,
 ):
     """Build the system hamiltonian."""
-    n_sites = l_x * (n_ph + 1)
-    h_matrix = np.zeros(
-        shape=(n_sites, n_sites),
+    g = gam / t
+    NN = Lx * (Nph + 1)
+    H = np.zeros(
+        shape=(NN, NN),
         dtype=complex,
     )
 
-    u_i = random_distribution(l_x, sigma_u, dist_kind, rng)
+    U = randomdistribution(Lx, sigmaU, distkind)
+    P, delta = funcoesP(Nph)
 
-    # Diagonal
-    for j in range(0, n_ph + 1):
-        for i in range(1, l_x + 1):
-            h_matrix[j * l_x + i, j * l_x + i] += j * omega + u_i[i]
+    # diagonal
+    print("escrevendo a matrix")
 
-    # Off-diagonal
-    for j_1 in range(0, n_ph + 1):
-        for j_2 in range(j_1, n_ph + 1):
-            for i in range(1):
-                h_matrix += 0.0
+    for j in range(0, Nph + 1):
+        for i in range(Lx):
+            H[j * Lx + i, j * Lx + i] = j * omega + U[i]
 
-    return h_matrix
+    # off-diagonal
+    print("escrevendo a matrix")
+
+    for j1 in range(0, Nph + 1):  #j1 = N
+        for j2 in range(j1, Nph + 1):  #j2 = M
+            hNM = 0.0
+            fats = 1
+            for s in range(0, j1 + 1):
+                fatj = 1
+                for j in range(0, j2 + 1):
+                    hNM += np.exp(-0.5 * g**2) * ((CI*g)**s) * ((CI*g)**j) * \
+                        delta[j1 - s, j2 - j] * P[s, j1] * P[j, j2] / (fats * fatj)
+                    fatj *= j + 1
+                fats *= s + 1
+            
+            for i in range(0, Lx - 1):
+                H[j2 * Lx + i, j1 * Lx + i + 1] = hNM * t
+                H[j2 * Lx + i + 1, j1 * Lx + i] = np.conj(hNM) * t
+
+                H[j1 * Lx + i + 1, j2 * Lx + i] = np.conj(hNM) * t
+                H[j1 * Lx + i, j2 * Lx + i + 1] = hNM * t
+
+    return H
 
 
 def main():
     """Main function."""
-    l_x, N_PH, SEED, DISTKIND = 20, 2, 1892, 1
-    T, GAM, SIGMA_U = 1.000, 0.0001, 0.200
-    TEMP, OMEGA = 0.0001, 0.500
-
-    SEED = 1892
-    RNG = np.random.default_rng(seed=SEED)
-
-    NN = l_x * (N_PH + 1)
+    h_mat = Hamiltonian(
+        Lx=4,
+        Nph=2,
+        t=1.0,
+        gam=0.1,
+        sigmaU=0.0,
+        omega=1.0,
+        distkind=0,
+    )
+    # np.savetxt(
+    #     fname="pytest.txt",
+    #     X=h_mat,
+    #     fmt="%.2e",
+    # )
     return None
 
 
---
+++ chain_alt.py
@@ -0,0 +1,204 @@
+"""Build and diagonalize the hamiltonian matrix
+of a 'single electron + cavity' system.
+"""
+import numpy as np
+from scipy.special import factorial, perm
+
+RNG = np.random.default_rng(seed=1892)
+CI = complex(0, 1)
+
+
+def random_distribution(
+    l_x: int,
+    sigma_u: float,
+    dist_kind: int,
+):
+    """Random distribution of the on-site energies."""
+    if dist_kind == 1:
+        u_onsite = RNG.normal(
+            scale=sigma_u,
+            size=l_x,
+        )
+    else:
+        u_onsite = RNG.uniform(
+            low=-0.5 * sigma_u,
+            high=0.5 * sigma_u,
+            size=l_x,
+        )
+    return u_onsite
+
+
+def p_func(
+    j: int,
+    m_ph: int,
+):
+    """Auxiliary function P(j, M)."""
+    p_sq = perm(
+        N=m_ph,
+        k=j,
+        exact=True,
+    )
+    return np.sqrt(p_sq)
+
+
+def h_nm(
+    n_ph: int,
+    n_row: int,
+    m_col: int,
+    g_param: float,
+):
+    """Auxiliary function h_NM(g)"""
+    delta = np.eye(n_ph + 1)
+    accum_sum = complex(0)
+    for i, j in np.ndindex(n_row + 1, m_col + 1):
+        accum_prod = complex(1)
+        fat_i = factorial(n=i, exact=True)
+        fat_j = factorial(n=j, exact=True)
+
+        accum_prod *= delta[n_row - i, m_col - j]
+        accum_prod *= (CI * g_param) ** i / fat_i
+        accum_prod *= (CI * g_param) ** j / fat_j
+        accum_prod *= p_func(j, m_col) * p_func(i, n_row)
+
+        accum_sum += accum_prod
+
+    return np.exp(-(g_param**2) / 2) * accum_sum
+
+
+def field_block_hamiltonian(
+    l_x: int,
+    n_ph: int,
+    n_row: int,
+    m_col: int,
+    omega: float,
+):
+    """Build the field block hamiltonian"""
+    delta = np.eye(n_ph + 1)
+    id_matrix = np.identity(
+        n=l_x,
+        dtype=complex,
+    )
+    h_field = id_matrix * delta[n_row, m_col] * n_row * omega
+    return h_field
+
+
+def chain_diag_block_hamiltonian(
+    n_ph: int,
+    n_row: int,
+    m_col: int,
+    u_onsite: np.ndarray,
+):
+    """Build the chain diagonal block hamiltonian"""
+    delta = np.eye(n_ph + 1)
+    h_chain = np.array(
+        object=delta[n_row, m_col] * np.diagflat(u_onsite),
+        dtype=complex,
+    )
+    return h_chain
+
+
+def chain_offdiag_block_hamiltonian(
+    l_x: int,
+    n_ph: int,
+    n_row: int,
+    m_col: int,
+    t_hop: float,
+    gamma: float,
+):
+    """Build the chain diagonal block hamiltonian"""
+    g_param = gamma / t_hop
+
+    aux_h_pg = h_nm(
+        n_ph,
+        n_row,
+        m_col,
+        g_param,
+    )
+    h_chain = t_hop * np.diagflat(
+        v=aux_h_pg * np.ones(l_x - 1, dtype=complex),
+        k=1,
+    )
+
+    aux_h_mg = h_nm(
+        n_ph,
+        n_row,
+        m_col,
+        -g_param,
+    )
+    h_chain += t_hop * np.diagflat(
+        v=aux_h_mg * np.ones(l_x - 1, dtype=complex),
+        k=-1,
+    )
+
+    return h_chain
+
+
+def hamiltonian(
+    l_x: int,
+    n_ph: int,
+    t_hop: float,
+    gamma: float,
+    sigma_u: float,
+    omega: float,
+    dist_kind: int = 0,
+):
+    """Build the system hamiltonian"""
+    delta = np.eye(n_ph + 1)
+    # g = gamma / t_hop
+
+    u_onsite = random_distribution(
+        l_x,
+        sigma_u,
+        dist_kind,
+    )
+
+    n_n = l_x * (n_ph + 1)
+    h_matrix = np.zeros(shape=(n_n, n_n), dtype=complex)
+    for n_row, m_col in np.ndindex(n_ph + 1, n_ph + 1):
+        h_block = np.zeros(shape=(l_x, l_x), dtype=complex)
+
+        h_block += field_block_hamiltonian(l_x, n_ph, n_row, m_col, omega)
+        h_block += chain_diag_block_hamiltonian(n_ph, n_row, m_col, u_onsite)
+        h_block += chain_offdiag_block_hamiltonian(
+            l_x, n_ph, n_row, m_col, t_hop, gamma
+        )
+
+        h_matrix += np.kron(
+            a=np.outer(delta[:, n_row], delta[:, m_col]),
+            b=h_block,
+        )
+
+    return h_matrix
+
+
+def main():
+    """Main function."""
+    # l_x, n_ph, seed, distkind = 20, 2, 1892, 1
+    # t, gam, sigma_u = 1.000, 0.0001, 0.200
+    # TEMP, OMEGA = 0.0001, 0.500
+
+    # NN = l_x * (n_ph + 1)
+
+    matrix = hamiltonian(
+        l_x=4,
+        n_ph=2,
+        t_hop=1.0,
+        gamma=0.1,
+        sigma_u=1.0,
+        omega=0.0,
+    )
+    print(matrix, end="\n\n")
+
+    eigvals, eigvecs = np.linalg.eigh(a=matrix, UPLO="U")
+    print(eigvals, end="\n\n")
+    print(eigvecs, end="\n\n")
+
+    np.savetxt(
+        fname="pytest_alt.txt",
+        X=hamiltonian(l_x=4, n_ph=2, t_hop=1.0, gamma=0.1, sigma_u=0.0, omega=1.0),
+        fmt="%.2e",
+    )
+
+
+if __name__ == "__main__":
+    main()
