80eb4827c850603daad2b0bd11abc8fa846531ad
Thales Freitas Macedo <thalesfreitas96@gmail.com>
2023-10-07 20:10:10 -0300

    Program now is one file only

--- chain.py
+++ chain.py
@@ -1,9 +1,10 @@
-"""Build and diagonalize the hamiltonian matrix
+"""Build the hamiltonian matrix and calculate the transmittance
 of a 'single electron + cavity' system.
 """
 import sys
 import numpy as np
 from scipy.special import factorial, perm
+from tqdm.auto import trange
 
 # np.set_printoptions(
 #     precision=1,
@@ -17,14 +18,30 @@ CI = complex(
     real=0,
     imag=1,
 )
-PI = np.pi
+# PI = np.pi
+
+N_A = 80  # Number of sites
+N_PH = 0  # Number of photons
+DIST_KIND = 1  # = 1 -> Gaussian, =/= 1 -> Uniform
+NE_POINTS = 200  # Energy grid
+NUM_DISORDER = 100  # Number of disorder configurations
+T_CH = 1.0  # Hopping inside chain
+GAMMA = 0.0  # Coupling constant
+DELTA_U = 0.5
+OMEGA = 2.0  # Photon frequency
+T_CS = 1.0  # Hopping between source and channel
+T_CD = 1.0  # Hopping between drain and channel
+T_LS = 1.0  # Hopping inside source
+T_LD = 1.0  # Hopping inside drain
+MU_S = 0.0  # Source chemical potential
+MU_D = 0.0  # Drain chemical potential
 
 
 def random_distribution(
-    n_a: int,
-    delta_u: float,
-    num_disorder: int,
-    dist_kind: int,
+    n_a: int = N_A,
+    delta_u: float = DELTA_U,
+    num_disorder: int = NUM_DISORDER,
+    dist_kind: int = DIST_KIND,
 ):
     """Random distribution of the on-site energies."""
     if dist_kind == 1:
@@ -44,7 +61,7 @@ def random_distribution(
 def p_func(
     j: int,
     m_ph: int,
-):
+) -> float:
     """Auxiliary function P(j, M)."""
     p_squared = perm(
         N=m_ph,
@@ -67,7 +84,7 @@ def h_nm(
         accum_prod = complex(1)
 
         accum_prod *= delta[n_row - i, m_col - j]
-        accum_prod *= (CI * g_param) ** i / factorial(i)  # Meu cálculo deu diferente
+        accum_prod *= (CI * g_param) ** i / factorial(i)
         accum_prod *= (CI * g_param) ** j / factorial(j)
         accum_prod *= p_func(j, m_col) * p_func(i, n_row)
 
@@ -118,26 +135,13 @@ def chain_offdiag_block(
         m_col,
         g_param,
     )
-    # aux_h_pg = h_nm(
-    #     n_ph,
-    #     n_row,
-    #     m_col,
-    #     g_param,
-    # )
+
     h_chain = t_ch * np.diagflat(
-        # v=aux_h_pg * np.ones(n_a - 1, dtype=complex),
         v=array_h_nm,
         k=1,
     )
 
-    # aux_h_mg = h_nm(
-    #     n_ph,
-    #     n_row,
-    #     m_col,
-    #     -g_param,
-    # )
     h_chain += t_ch * np.diagflat(
-        # v=aux_h_mg * np.ones(n_a - 1, dtype=complex),
         v=np.conj(array_h_nm),
         k=-1,
     )
@@ -147,7 +151,6 @@ def chain_offdiag_block(
 
 def block_over(
     block_matrix: np.ndarray,
-    # n_a: int,
     n_ph: int,
     ph_row: int,
     ph_col: int,
@@ -165,26 +168,13 @@ def block_over(
 
 def hamiltonian(
     u_onsite: np.ndarray,  # On-site energies
-    n_a: int = 80,  # Number of sites
-    n_ph: int = 0,  # Number of photons
-    t_ch: float = 1.0,  # Hopping inside chain
-    gamma: float = 0.0,  # Coupling constant
-    # delta_u: float = 0.0,  # Disorder standard deviation
-    omega: float = 2.0,  # Photon frequency
-    # dist_kind: int = 1,  # Distribution kind
+    n_a: int = N_A,
+    n_ph: int = N_PH,
+    t_ch: float = T_CH,
+    gamma: float = GAMMA,
+    omega: float = OMEGA,
 ):
     """Build the chain hamiltonian."""
-    # delta = np.identity(
-    #     n=n_ph + 1,
-    #     dtype=complex,
-    # )
-
-    # u_onsite = random_distribution(
-    #     n_a,
-    #     delta_u,
-    #     dist_kind,
-    # )
-
     n_n = n_a * (n_ph + 1)
     h_matrix = np.zeros(shape=(n_n, n_n), dtype=complex)
 
@@ -194,18 +184,11 @@ def hamiltonian(
         h_block += field_block(n_a, n_diag, omega)
         h_block += chain_diag_block(u_onsite)
 
-        # h_matrix += np.kron(
-        #     a=np.outer(delta[:, n_diag], delta[:, n_diag]),
-        #     b=h_block,
-        # )
-
         h_matrix += block_over(h_block, n_ph, n_diag, n_diag)
 
     # Off-diagonal subblocks
     for n_row, m_col in np.ndindex(n_ph + 1, n_ph + 1):
         h_block = np.zeros(shape=(n_a, n_a), dtype=complex)
-        # h_block += field_block_hamiltonian(l_x, n_ph, n_row, m_col, omega)
-        # h_block += chain_diag_block_hamiltonian(n_ph, n_row, m_col, u_onsite)
         h_block += chain_offdiag_block(
             n_a,
             n_ph,
@@ -215,11 +198,6 @@ def hamiltonian(
             gamma,
         )
 
-        # h_matrix += np.kron(
-        #     a=np.outer(delta[:, n_row], delta[:, m_col]),
-        #     b=h_block,
-        # )
-
         h_matrix += block_over(h_block, n_ph, n_row, m_col)
 
     return h_matrix
@@ -227,21 +205,20 @@ def hamiltonian(
 
 def f_func(arg: float) -> complex:
     """Auxiliary function f(x)"""
-    # return 2 / (arg + CI * np.sqrt(4 - arg**2))
     return 0.5 * (arg - CI * np.sqrt(4 - arg**2))
 
 
 def sigma_lead(
     energy: float,
     lead: str,
-    n_a: int = 80,
-    n_ph: int = 0,
-    t_cs: float = 1.0,  # Hopping between source and channel
-    t_cd: float = 1.0,  # Hopping between drain and channel
-    t_ls: float = 1.0,  # Hopping inside source
-    t_ld: float = 1.0,  # Hopping inside drain
-    mu_s: float = 0.0,
-    mu_d: float = 0.0,
+    n_a: int = N_A,
+    n_ph: int = N_PH,
+    t_cs: float = T_CS,
+    t_cd: float = T_CD,
+    t_ls: float = T_LS,
+    t_ld: float = T_LD,
+    mu_s: float = MU_S,
+    mu_d: float = MU_D,
 ):
     """Build the retarded drain self-energy matrix."""
     block_matrix = np.zeros(
@@ -254,21 +231,9 @@ def sigma_lead(
     elif lead == "d":
         block_matrix[-1, -1] = t_cd**2 / t_ld * f_func((energy - mu_d) / t_ld)
 
-    # print(block_matrix)
-
-    # delta = np.identity(
-    #     n=n_ph + 1,
-    #     dtype=complex,
-    # )
     n_n = n_a * (n_ph + 1)
     matrix = np.zeros(shape=(n_n, n_n), dtype=complex)
-    # for n_row, m_col in np.ndindex(n_ph + 1, n_ph + 1):
     for n_row, m_col in [(0, 0)]:
-        # matrix += np.kron(
-        #     a=np.outer(delta[:, n_row], delta[:, m_col]),
-        #     b=block_matrix,
-        # )
-
         matrix += block_over(block_matrix, n_ph, n_row, m_col)
 
     return matrix
@@ -297,7 +262,7 @@ def transmittance(
     green_ret: np.ndarray,
     gamma_s: np.ndarray,
     gamma_d: np.ndarray,
-):
+) -> float:
     """Calculate the transmittance for the given self-energies and Green's function."""
     green_adv = c_t(green_ret)
     return np.real(np.trace(gamma_s @ green_ret @ gamma_d @ green_adv))
@@ -305,21 +270,21 @@ def transmittance(
 
 def write_input(
     return_array: np.ndarray,
-    n_a: int,
-    n_ph: int,
-    dist_kind: int,
-    ne_points: int,
-    num_disorder: int,
-    t_ch: float,
-    gamma: float,
-    delta_u: float,
-    omega: float,
-    t_cs: float,
-    t_cd: float,
-    t_ls: float,
-    t_ld: float,
-    mu_s: float,
-    mu_d: float,
+    n_a: int = N_A,
+    n_ph: int = N_PH,
+    dist_kind: int = DIST_KIND,
+    ne_points: int = NE_POINTS,
+    num_disorder: int = NUM_DISORDER,
+    t_ch: float = T_CH,
+    gamma: float = GAMMA,
+    delta_u: float = DELTA_U,
+    omega: float = OMEGA,
+    t_cs: float = T_CS,
+    t_cd: float = T_CD,
+    t_ls: float = T_LS,
+    t_ld: float = T_LD,
+    mu_s: float = MU_S,
+    mu_d: float = MU_D,
 ):
     """Write input."""
 
@@ -331,20 +296,68 @@ def write_input(
         print("Input data")
         print("Lx=", n_a, "Nph=", n_ph, "seed=", SEED)
         print("Distribution (1 Gaussian, any other retangular)=", dist_kind)
-        print("Energy grid=", ne_points, "Number of disorder conf.=", num_disorder)
-        print("t=", t_ch, "gam=", gamma, "sigma (dist)=", delta_u, "Omega=", omega)
+        print("Energy grid=", ne_points,
+              "Number of disorder conf.=", num_disorder)
+        print("t=", t_ch, "gam=", gamma,
+              "sigma (dist)=", delta_u, "Omega=", omega)
         print("tcS=", t_cs, "tcD=", t_cd, "tlS=", t_ls, "tlD=", t_ld)
         print("muD=", mu_s, "muD=", mu_d)
 
     np.savetxt(
         fname="dados" + filename,
         X=return_array.T,
+        # delimiter="        ",
     )
 
 
-# def main():
-#     """Main function."""
+def main():
+    """Main function."""
+    # On-site energies for multiple disorder configs
+    array_u_onsite = random_distribution()
+
+    return_array = np.zeros(
+        shape=(3, NE_POINTS),
+        dtype=float,
+    )
+    return_array[0, :] = np.linspace(
+        start=-2.0,
+        stop=2.0,
+        num=NE_POINTS + 2,
+    )[1:-1]
+
+    transmittances = np.zeros(
+        shape=(NUM_DISORDER, NE_POINTS),
+        # dtype=complex,
+    )
+
+    for i in trange(NUM_DISORDER, desc="Desordem"):
+        h_matrix = hamiltonian(array_u_onsite[i])
+
+        # for ne in trange(NE_POINTS, desc="Transmitância"):
+        for ne in range(NE_POINTS):
+            sigma_s = sigma_lead(return_array[0, ne], "s")
+            sigma_d = sigma_lead(return_array[0, ne], "d")
+            sigma_l = sigma_s + sigma_d
+
+            green_f = green_function(return_array[0, ne], h_matrix, sigma_l)
+
+            gamma_s = CI * (sigma_s - c_t(sigma_s))
+            gamma_d = CI * (sigma_d - c_t(sigma_d))
+
+            transmittances[i, ne] = transmittance(green_f, gamma_s, gamma_d)
+
+    return_array[1, :] = np.mean(
+        a=transmittances,
+        axis=0,
+    )
+    return_array[2, :] = np.nanstd(
+        a=transmittances,
+        axis=0,
+        ddof=1,
+    )
+
+    write_input(return_array)
 
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
